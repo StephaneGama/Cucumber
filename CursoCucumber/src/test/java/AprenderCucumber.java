77feff7bf7b3489a302cdb03ef8b1e242390780c
StephaneGama <stephane.matosgama@hotmail.com>
2021-07-05 21:57:10 -0300

    Finalizando Aula 11

--- CursoCucumber/src/test/java/AprenderCucumber.java
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+
 import cucumber.api.java.en.*;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
@@ -20,6 +22,29 @@ import cucumber.api.java.pt.Quando;
     	public void finalizar() throws Throwable {
     		System.out.println("Qualquer coisa3");
     	}  
+    	
+    	
+    	private int contador = 0;
+    	
+    	@Dado("^que o valor do contador é (\\d+)$")
+    	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+    		contador = arg1;
+ 
+    	}
+
+    	@Quando("^eu incrementar em (\\d+)$")
+    	public void euIncrementarEm(int arg1) throws Throwable {
+    	    contador = contador + arg1;
+    	}
+
+    	@Então("^o valor do contador será (\\d+)$")
+    	public void oValorDoContadorSerá(int arg1) throws Throwable {
+    	    Assert.assertEquals(arg1, contador);
+    	   // throw new RuntimeException();
+    	    
+    	    
+    	}
+
     }
 
 
--- CursoCucumber/src/test/java/aprender_cocumber.feature
+++ CursoCucumber/src/test/java/aprender_cocumber.feature
@@ -6,8 +6,12 @@
       Para que eu possa automatizar
      
       Cenario: Deve executar especificacao
-     
-        Dado criar arquivo
-        Quando executalo
-        Entao finalizar
-        
+      Dado criar arquivo
+      Quando executalo
+      Entao finalizar
+       
+      Cenário: Deve incrementar contador
+      Dado que o valor do contador é 15
+      Quando eu incrementar em 3
+      Então o valor do contador será 18
+      
